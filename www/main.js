9da3530f189a7ccd6cc932597ebf17569f83efe2
Brandon Valosek <bvalosek@gmail.com>
2012-12-23 15:12:52 -0600

    New stuff for Font Awsesome and keymage + main fixes

--- www/main.js
+++ www/main.js
@@ -12,21 +12,26 @@ require.config({
         'backbone'     : 'vendor/backbone',
         'text'         : 'vendor/text',
         'require-css'  : 'vendor/require-css',
-        'require-less' : 'vendor/require-less'
+        'require-less' : 'vendor/require-less',
+        'keymage'      : 'vendor/keymage',
+        'font-awesome' : 'vendor/font-awesome'
     },
 
     // request translations
     map: {
         '*': {
-            'css'  : 'require-css/css',
-            'less' : 'require-less/less',
+            'css'       : 'require-css/css',
+            'less'      : 'require-less/less',
+
+            // require for css-builder hack
+            'normalize' : 'require-css/normalize'
         }
     },
 
     // build opts
     out                     : 'main.built.js',
+    skipDirOptimize         : true,
     name                    : 'main',
-    include                 : ['css', 'manifest', 'require-css/normalize'],
 
     optimize                : 'uglify2',
 
@@ -43,7 +48,7 @@ require.config({
 require(
 
     // initial modules to load
-    ['example/SampleApplication', 'manifest'],
+    ['example/SampleApplication', 'manifest', 'css'],
 
     // kick it off
     function(Application, manifest) {
