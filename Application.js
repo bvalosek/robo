5abb3a4afebeba6bc9fdd9d7c45ba818874190ea
Brandon Valosek <bvalosek@gmail.com>
2013-02-25 22:18:42 -0600

    new style

--- Application.js
+++ Application.js
@@ -6,72 +6,75 @@ define(function(require, exports, module) {
     var withEvents     = require('./mixins/withEvents');
     var Backbone       = require('backbone');
 
-    var Application = Base.extend(function() {
-        this.onCreate();
+    var Application = Base.extend({
 
-        this.window = new View()
-            .setElement('body')
-            .mixin(asCompositable);
+        constructor: function() {
+            this.onCreate();
 
-        var d = this.onStart();
+            this.window = new View()
+                .setElement('body')
+                .mixin(asCompositable);
 
-        // how we resume
-        var postStart = function() {
-            this.onResume();
+            var d = this.onStart();
 
-            // set that glob
-            window.theApp = this;
-        }.bind(this);
+            // how we resume
+            var postStart = function() {
+                this.onResume();
 
-        // if application is still starting
-        if (d && d.then)
-            d.then(postStart);
-        else
-            postStart();
+                // set that glob
+                window.theApp = this;
+            }.bind(this);
+
+            // if application is still starting
+            if (d && d.then)
+                d.then(postStart);
+            else
+                postStart();
+        },
+
+        // proxy events to the window renderable
+        on: function(eventName, fn, context)
+        {
+
+            var f = fn.bind(context || this);
+            return this.window.on(eventName, function() {
+                f(arguments[1]);
+            });
+        },
+
+        // clobber trigger and pipe events via dom
+        trigger: function()
+        {
+            return this.window.trigger.apply(this.window.trigger, arguments);
+        },
+
+        // will start routing immediately based on current URL
+        startRouter: function(RouterClass)
+        {
+            this.router = new RouterClass(this);
+            Backbone.history.start({ pushState: true });
+        },
+
+        // add a view to the overall DOM and add a class so we can alter the other
+        // parts of the screen
+        addPopup: function(view)
+        {
+            var cName = view.getClassName() + '-open';
+
+            this.window.addView(view);
+            this.window.addClass(cName);
+
+            // remove the popup flag when it closes
+            view.on('close', function() {
+                this.window.removeClass(cName);
+            }.bind(this));
+        },
+
+        // lifecycle defaults-- should override
+        onCreate : function() {},
+        onStart  : function() {},
+        onResume : function() {}
     });
-
-    // proxy events to the window renderable
-    Application.prototype.on = function(eventName, fn, context)
-    {
-
-        var f = fn.bind(context || this);
-        return this.window.on(eventName, function() {
-            f(arguments[1]);
-        });
-    };
-
-    // clobber trigger and pipe events via dom
-    Application.prototype.trigger = function()
-    {
-        return this.window.trigger.apply(this.window.trigger, arguments);
-    };
-
-    // will start routing immediately based on current URL
-    Application.prototype.startRouter = function(RouterClass)
-    {
-        this.router = new RouterClass(this);
-        Backbone.history.start({ pushState: true });
-    };
-
-    // add a view to the overall DOM and add a class so we can alter the other
-    // parts of the screen
-    Application.prototype.addPopup = function(view)
-    {
-        var cName = view.getClassName() + '-open';
-
-        this.window.addView(view);
-        this.window.addClass(cName);
-
-        // remove the popup flag when it closes
-        view.on('close', function() {
-            this.window.removeClass(cName);
-        }.bind(this));
-    };
-
-    // lifecycle
-    Application.prototype.onCreate = function() {};
-    Application.prototype.onStart  = function() {};
-    Application.prototype.onResume = function() {};
 
     return Application;
 });
