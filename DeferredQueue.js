525be165e85856aee30fc1e9467ec2a924754194
Brandon Valosek <bvalosek@gmail.com>
2013-03-07 13:06:39 -0600

    DeferredQueue object for keeping multiple calls that return promises in sync

---
+++ DeferredQueue.js
@@ -0,0 +1,67 @@
+define(function(require, exports, module) {
+
+    var Base = require('./Base');
+    var _    = require('underscore');
+    var $    = require('jquery');
+
+    // queue up functions taht return a deferred object, ensuring that they happen
+    // sequentially
+    var DeferredQueue = Base.extend({
+
+        // optionally provide a single function that is being queued
+        constructor: function(fn, context)
+        {
+            this._queue = [];
+            this._fn = context ? fn.bind(context) : fn;
+        },
+
+        queue: function()
+        {
+            var args = _(arguments).toArray();
+            var d    = new $.Deferred();
+
+            this._queue.push({
+                args: args,
+                promise: d
+            });
+
+            this._runQueue();
+
+            return d;
+        },
+
+        _runQueue: function()
+        {
+            // when the queue is empty, allow it to be run again later
+            if (!this._queue.length) {
+                this._running = false;
+                return;
+            }
+
+            if (this._running)
+                return;
+
+            this._running = true;
+
+            var info = this._queue.splice(0, 1)[0];
+            var args = info.args;
+
+            var d = this._fn.apply(this, args);
+
+            // call when the running fn finishes or after the stack clears if
+            // it's not a deferred object
+            if (d && d.then) {
+                d.then(function(retValue) {
+                    this._runQueue();
+                    info.promise.resolve(retValue);
+                }.bind(this));
+            } else {
+                info.promise.resolve();
+            }
+        }
+
+    });
+
+    return DeferredQueue;
+
+});
