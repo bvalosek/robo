381d4d6f4f166eefb534fb7275fa44f9c5e61410
Brandon Valosek <bvalosek@gmail.com>
2013-03-09 14:27:58 -0600

    model changes

--- Model.js
+++ Model.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
 
-    var compose = require('./compose');
+    var compose    = require('./compose');
+    var Collection = require('./Collection');
 
     // create BackboneModel that has the compose.js goodies baked in
     var BackboneModel = Backbone.Model.extend();
@@ -8,7 +9,37 @@ define(function(require, exports, module) {
 
     var Model = BackboneModel.extend({
 
+        baseCheck: function()
+        {
+            console.log('check');
+        }
+
     });
+
+    // have to create our own extender to ensure the Collection object also
+    // gets extended
+    var makeExtender = function(parentExtender, parentCollection)
+    {
+        return function(obj) {
+            Child = parentExtender.apply(this, arguments);
+
+            obj = obj || {};
+
+            var cHash = _(obj.Collection || {}).extend({
+                url: obj.urlRoot || '',
+                model: Child
+            });
+
+            Child.Collection = parentCollection.extend(cHash);
+
+            return Child;
+        };
+    };
+
+    // Static class let's us make collections via model classes
+    Model.Collection = Collection;
+
+    Model.extend = makeExtender(Model.extend, Model.Collection);
 
     return Model;
 });
