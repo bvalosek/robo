417dae9f06d30dbe6e59ddde784cfb3f3846c2c6
Dillon Shook <dshook@alumni.nmt.edu>
2013-03-19 14:22:32 -0500

    Basic tests, more to come

--- tests/test.js
+++ tests/test.js
@@ -2,15 +2,48 @@ define(function(require, exports, module) {
 
     var compose = require('robo/compose');
 
-    var C = compose.defineClass({
+    
 
-        constructor: function()
+    var BaseClass = compose.defineClass({
+
+        __virtual__render: function()
         {
-            console.log('hello world');
-        }
+            return "base render";
+        },
+        __virtual__baseMethod: function ()
+        {
+            return "base method";
+        },
+
+        basePlain: 1,
+        __virtual__baseVirtual: 2,
+        __abstract__baseAbstract: undefined,
 
     });
 
-    new C();
+    var ChildClass = BaseClass.extend({
+        __override__render: function ()
+        {
+            return "child render";
+        },
+        childMethod: function ()
+        {
+            return "child method";
+        },
 
+        __override__baseVirtual: -2,
+        __override__baseAbstract: -3,
+    });
+
+    test("Basic Inheritance", function ()
+    {
+        var child = new ChildClass();
+        equal(child.render(), "child render");
+        equal(child.childMethod(), "child method");
+        equal(child.baseMethod(), "base method");
+        equal(child.basePlain, 1);
+        equal(child.baseVirtual, -2);
+        equal(child.baseAbstract, -3);
+
+    });
 });
