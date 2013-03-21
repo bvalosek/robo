4a89f92211800093039454a7b6ebd2b8873eb92c
Brandon Valosek <bvalosek@gmail.com>
2013-03-21 15:36:38 -0500

    __SuperDuper__ tests

--- test/unit/bmixins.js
+++ test/unit/bmixins.js
@@ -26,13 +26,18 @@ define(function(require) {
         var ThingAgain = Thing.extend({ __name__  : 'ThingAagain' });
 
         // --------------------------------------------------------------------
+        // sigs
+
+        console.log(helpers.prettySig(Thing));
+        console.log(helpers.prettySig(ThingAgain));
+
+        // --------------------------------------------------------------------
         // tests
 
         raises(function() {
             var a = new IThing();
         }, null, 'Prevent instantiation of abstract class');
 
-        console.log(helpers.prettySig(Thing));
         deepEqual(
             Thing.findAnnotations('fn'),
             {ABSTRACT: true, MIXIN: true},
@@ -48,11 +53,55 @@ define(function(require) {
             {},
             'no annotations on mixed-on-top-off matching function');
 
-        console.log(helpers.prettySig(ThingAgain));
         deepEqual(
             Thing.getSignature(),
             ThingAgain.getSignature(),
             'Empty extend yields same signature');
+
+        equal(helpers.isAbstract(Thing), true, 'Class from interface is abstract');
+
+    });
+
+    test('Mixin Super vs __SuperDuper__ propigation', function() {
+
+        // --------------------------------------------------------------------
+        // setup classes
+
+        var Child1 = compose.Object.extend({ __name__: 'Child1' });
+        var Child2 = Child1.extend({ __name__: 'Child2' });
+        var Child3 = Child2.extend({ __name__: 'Child3' });
+
+        var MixA = compose.defineMixin({ __name__: 'MixA' });
+        var MixB = compose.defineMixin({ __name__: 'MixB' });
+        var MixC = compose.defineMixin({ __name__: 'MixC' });
+
+        var Child2A = Child2.using(MixA);
+        var Child2B = Child2.using(MixB);
+
+        // --------------------------------------------------------------------
+        // sigs
+
+        console.log('\n\n');
+        console.log(helpers.prettySig(Child1));
+        console.log(helpers.prettySig(Child2));
+        console.log(helpers.prettySig(Child3));
+        console.log(helpers.prettySig(Child2A));
+        console.log(helpers.prettySig(Child2B));
+
+        ok(new Child1().is(compose.Object), 'Child1 instance is a Object');
+        ok(new Child2().is(Child1), 'Child2 instance is a Child1');
+        ok(new Child3().is(Child1), 'Child3 instance is a Child1');
+        ok(!new Child1().is(Child3), 'Child1 instance is not a Child3');
+
+        ok(Child1.Super === compose.Object, 'Child1 Super is Parent');
+        ok(Child2.Super === Child1, 'Child1 Super is Parent');
+        ok(Child3.Super === Child2, 'Child1 Super is Parent');
+
+        ok(Child2A.Super === Child2.Super, 'Bare mixin class should leave Super intact');
+        ok(Child2A.Super === Child2B.Super, 'Bare mixin classes from same base class should have same Super');
+
+        ok(Child2A.__SuperDuper__.Super === Child2.Super, 'Bare mixin class __SuperDuper__\'s Super should be the faked Super');
+
     });
 
 });
