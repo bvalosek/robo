48325b6e3ab232418d364443bcf97e8bfc6306d4
Brandon Valosek <bvalosek@gmail.com>
2013-03-21 16:35:09 -0500

    using better asserts

--- test/unit/api.js
+++ test/unit/api.js
@@ -13,16 +13,16 @@ define(function(require) {
 
         for (var n in check) {
             var O = check[n];
-            ok(_(O.extend).isFunction(), 'Object#extend is function: level ' + n);
-            ok(_(O.using).isFunction(), 'Object#using is function: level ' + n);
-            ok(_(O.findAnnotations).isFunction(), 'Object#findAnnotations is function: level ' + n);
-            ok(_(O.findMembers).isFunction(), 'Object#findMembers is function: level ' + n);
-            ok(_(O.prototype.is).isFunction(), 'Object.prototype#is is function: level ' + n);
-            ok(_(O.prototype.mixin).isFunction(), 'Object.prototype#mixin is function: level ' + n);
+            strictEqual(_(O.extend).isFunction(), true, 'Object#extend is function: level ' + n);
+            strictEqual(_(O.using).isFunction(), true, 'Object#using is function: level ' + n);
+            strictEqual(_(O.findAnnotations).isFunction(), true, 'Object#findAnnotations is function: level ' + n);
+            strictEqual(_(O.findMembers).isFunction(), true, 'Object#findMembers is function: level ' + n);
+            strictEqual(_(O.prototype.is).isFunction(), true, 'Object.prototype#is is function: level ' + n);
+            strictEqual(_(O.prototype.mixin).isFunction(), true, 'Object.prototype#mixin is function: level ' + n);
 
             // constructor stuff
-            ok(O === O.prototype.constructor, 'constructor is correct on prototype level ' + n);
-            ok(new O().is(O), 'object is its own self: level ' + n);
+            strictEqual(O, O.prototype.constructor, 'constructor is correct on prototype level ' + n);
+            strictEqual(new O().is(O), true, 'object is its own self: level ' + n);
 
             // ensure the object is empty from enumerable things
             deepEqual(_(O).keys(), [], 'no enumerable properites on object level ' + n);
