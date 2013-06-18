f0d46bc8c36d4dd016e00611a24ca5990f82060e
Brandon Valosek <bvalosek@gmail.com>
2013-06-18 13:11:10 -0500

    Observables

--- src/event/WithEvents.js
+++ src/event/WithEvents.js
@@ -12,9 +12,32 @@ define(function(require, exports, module) {
         initEvents: function()
         {
             var _this = this;
+
             _(this.constructor.__annotations__).each(function(a, key) {
+
+                // listen for any events
                 if (a.EVENT) {
                     _this.on(key, _this[key], _this);
+                }
+
+                // create observable members
+                if (a.OBSERVABLE) {
+                    _this._observables      = _this.observables || {};
+                    _this._observables[key] = _this[key];
+
+                    // install the getter/setter
+                    Object.defineProperty(_this, key, {
+                        configurable: true, enumberable: true,
+                        get: function() {
+                            return _this._observables[key];
+                        },
+                        set: function(v) {
+                            if (v === _this._observables[key]) return;
+                            _this._observables[key] = v;
+                            _this.trigger('change');
+                            _this.trigger('change:' + key);
+                        }
+                    });
                 }
             });
         },
