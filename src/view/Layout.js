66f26e85f111f657378a47a9b2127eeb96371b77
Brandon Valosek <bvalosek@gmail.com>
2013-07-03 07:53:48 -0500

    Handling of re-binding

--- src/view/Layout.js
+++ src/view/Layout.js
@@ -19,6 +19,7 @@ define(function(require) {
 
         __virtual__template: '',
 
+        // Allow us to set our data context and update all of our bindings
         __property__dataContext:
         {
             get: function() { return this._dataContext; },
@@ -29,30 +30,37 @@ define(function(require) {
         {
             this._dataContext = context;
             var _this = this;
-            console.log(context.constructor.__name__);
+
+            console.log('New data context for ' + this);
 
             // Instantiate and attach and widgets we have specified
             _(this.element.querySelectorAll('[data-robo-class]'))
                 .each(function(element) {
+
+                    // Get the module path from the attribute, must be loaded
+                    // somewhere else with a proper require() call
                     var mod = element
                         .getAttribute('data-robo-class')
                         .replace(/\./g, '/');
-
                     var C      = require(mod);
                     var widget = new C().setElement(element);
-
                     _this.widgets.push(widget);
 
-                    // do we need to set data context?
-                    var actualContext = context;
+                    // If this widget is some kind of layout (manages its own
+                    // stuff, overridable datacontext, etc
                     if (compose.is(widget, Layout)) {
                         var sContext = element.getAttribute('data-robo-context');
                         if (sContext) {
                             var dc = Layout.resolvePath(context, sContext);
-                            actualContext = dc.obj[dc.key];
-                        }
 
-                        widget.setDataContext(actualContext);
+                            // It's coming from our viewModel so it "should" be
+                            // observable, let's create a binding
+                            new Binding()
+                                .setSource(dc.obj, dc.key)
+                                .setTarget(widget, 'dataContext');
+                        } else {
+                            widget.setDataContext(context);
+                        }
                     }
             });
 
@@ -61,9 +69,9 @@ define(function(require) {
             _(this.element.querySelectorAll('[data-robo-binding]'))
                 .each(function(element) {
 
-                    // ensure that if this element was already bound (via sub
-                    // layout) that we don't step on that shit
-                    if (element.roboBound) {
+                    // If this element was already bound by somebody else, then
+                    // return. If it was bound by us, we are re-binding
+                    if (element.boundBy !== undefined && element.boundBy !== _this) {
                         return;
                     }
 
@@ -82,7 +90,7 @@ define(function(require) {
                             );
                     });
 
-                    element.roboBound = true;
+                    element.boundBy = _this;
             });
 
             return this;
