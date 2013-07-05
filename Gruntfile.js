97781601834a3c2f798ce9c26960a3773ba8e3ce
Brandon Valosek <bvalosek@gmail.com>
2013-07-05 14:57:12 -0500

    Stuff for test

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,27 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
 
+        pkg: grunt.file.readJSON('package.json'),
+
         jshint: {
             all: [
                 'src/**/*.js'
             ]
+        },
+
+        qunit: {
+            all: ['test/index.html']
         }
 
     });
 
     // plugins
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // tasks
     grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('default', ['lint']);
+    grunt.registerTask('test', ['lint', 'qunit']);
 
 };
