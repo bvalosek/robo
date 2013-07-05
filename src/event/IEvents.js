d400f2f9835fd9c0819bdc7324b84564bb54b899
Brandon Valosek <bvalosek@gmail.com>
2013-07-05 14:52:52 -0500

    IEvents update

--- src/event/IEvents.js
+++ src/event/IEvents.js
@@ -10,26 +10,28 @@ define(function(require) {
         // triggered, the callback is fired. Bind to an optional context. This
         // is used when we know that *something else* will trigger an event on
         // this object, and we want to react to it
-        __fluent__on : function(name, callback, _context) {},
+        __fluent__on : function(eventName, callback, _context) {},
 
         // Remove the event callbacks from this object. Will attempt to match
         // against the context, callback and event name if provided. The fewer
         // parameters provided, the more general the removal. Calling with no
         // paramters removes all event callbacks
-        __fluent__off : function(_name, _callback, _context) {},
+        __fluent__off : function(_eventName, _callback, _context) {},
 
         // Inversion-of-control version of 'on'. Let's this object keep track
         // of the objects it is listening to so that it is easier to clear out
         // the event callbacks on the other object. This is used when we know
         // another object is going to fire off an event, and we want to react
         // to it.
-        __fluent__listenTo : function(obj, name, callback) {},
-
+        __fluent__listenTo : function(otherObject, eventName, callback) {},
 
         // Corresponding to 'listenTo', clear out the event callbacks on other
         // objects to which this one is lisstening to. Like 'off', providing
         // more parameters will clear out more and more events.
-        __fluent__stopListening : function(_obj, _name, _callback) {}
+        __fluent__stopListening : function(_otherObject, _eventName, _callback) {},
+
+        // Fire off an event to all attached event callbacks on this object
+        __fluent__trigger: function(eventName, _eventParameters) {}
 
     });
 
