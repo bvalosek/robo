1539c5678ef632599c913764809103716a806491
Brandon Valosek <bvalosek@gmail.com>
2013-07-06 13:29:00 -0500

    More event tests

--- test/unit/event.js
+++ test/unit/event.js
@@ -22,8 +22,7 @@ define(function(require) {
         o.on('eventName', f);
         strictEqual(_(o._events).size(), 1, 'using on(name, cb) adds to _events');
         o.off('eventName', f);
-        strictEqual(_(o._events).size(), 0, 'using off(name, cb) removes event');
-        console.log(o._events.eventName);
+        strictEqual(_(o._events).size(), 0, 'using off(name, cb) removes event node when empty');
 
         // off with no args
         o = new EObject();
@@ -31,6 +30,27 @@ define(function(require) {
         o.off();
         deepEqual(o._events, {}, 'off() clears all');
 
+    });
+
+    test('Value of this when left unset', function() {
+
+        var o;
+        var event = 'event';
+
+        // using on
+        o = new EObject();
+        o.on(event, function() {
+            strictEqual(this, o, 'callback with on() is same as object');
+        });
+        o.trigger(event);
+
+        // using listenTo
+        o     = new EObject();
+        var p = new EObject();
+        p.listenTo(o, event, function() {
+            strictEqual(this, p, 'set with listenTo is object that is listening');
+        });
+        o.trigger(event);
 
     });
 
