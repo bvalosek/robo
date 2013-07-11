d2e926cadb75b42e9ae568716bdb3bda340cc827
Brandon Valosek <bvalosek@gmail.com>
2013-07-11 15:13:00 -0500

    example stuff

--- example/src/ExampleApp.js
+++ example/src/ExampleApp.js
@@ -1,18 +1,21 @@
-var Application      = require('../../lib/app/Application');
-var typedef          = require('typedef');
+var Application = require('../../lib/app/Application');
+var XmlResource = require('../../lib/util/XmlResource');
+var typedef     = require('typedef');
 
 module.exports = ExampleApp =
 typedef.class('ExampleApp').extends(Application).define({
 
     __event__onStart: function()
     {
-        global.app = this;
-
         // Setup route table
         this.addRoutes(require('./routes'));
 
         // Manually route for now
         this.route({ uri: '/' });
+
+        var config = require('./R')('config.xml');
+        console.log(config);
+        console.log(XmlResource.flattenResource(config));
     }
 
 });
