c52ede18912f7e4380b7f7ae978fd855b2ceeb5d
Brandon Valosek <bvalosek@gmail.com>
2013-07-21 15:20:04 -0500

    xamp

--- example/src/HomeViewModel.js
+++ example/src/HomeViewModel.js
@@ -8,30 +8,12 @@ module.exports = require('typedef')
 // View Model
 .class('HomeViewModel') .extends(ObservableObject) .define({
 
-    __observable__person    : null,
     __observable__people    : null,
-    __observable__userInput : '',
+    __observable__currentPerson : null,
 
     __constructor__: function()
     {
-        this.person = new Person();
         this.people = loader(R('people.xml'));
-    },
-
-    __observable__parsedInput: function()
-    {
-        var parts = this.userInput.split(' ');
-
-        var first = '', last = '';
-
-        if (parts.length == 1) {
-            first = parts[0];
-        } else if (parts.length == 2) {
-            first = parts[0];
-            last  = parts[1];
-        }
-
-        return { firstName: first, lastName: last };
     }
 
 });
--- example/src/main.js
+++ example/src/main.js
@@ -8,12 +8,6 @@ var vm   = global.vm   = new HomeViewModel();
 var view = global.view = loader(R('home.xml'), {element: document.body});
 var me   = global.me   = new Person();
 
-global.xme = loader(R('Me.xml'));
-
-me.firstName = 'Brandon';
-me.lastName = 'Valosek';
-
-vm.person.friends.add(me);
 
 view.setDataContext(vm);
 
